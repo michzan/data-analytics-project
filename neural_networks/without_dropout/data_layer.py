7005f5c631da86fedd0e32bfc44aee2e64447235
antonioIannotta <74740099+antonioIannotta@users.noreply.github.com>
2023-02-11 10:48:11 +0100

    feat: added comments to data layer

--- neural_networks/without_dropout/data_layer.py
+++ neural_networks/without_dropout/data_layer.py
@@ -5,7 +5,7 @@ from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 from torch.utils.data import Dataset
 
-
+# method used to load data from the several .csv files and to return the final dataframe
 def loadData():
     mean_rating = pd.read_csv('../../csv_genereted/mean_rating.csv')
     movie_genre = pd.read_csv('../../csv_genereted/movies_genre.csv')
@@ -16,7 +16,7 @@ def loadData():
 
     return final_df
 
-
+# method used for scaling and for the application of PCA
 def preprocess(X):
     standardScaler = StandardScaler()
     X = standardScaler.fit_transform(X)
@@ -24,6 +24,7 @@ def preprocess(X):
     return pca.fit_transform(X)
 
 
+# creation of the MovieLens dataset defining the torch tensors
 class MovieLensDataset(Dataset):
     def __init__(self):
         data = loadData()
